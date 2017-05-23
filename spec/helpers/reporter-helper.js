550e618fde7ebaf7b9f185169cbe8dc6c584e21c
mwnicks <matthew.nicks@methods.co.uk>
2017-05-23 13:53:58 +0100

    break between pages

--- spec/helpers/reporter-helper.js
+++ spec/helpers/reporter-helper.js
@@ -2,37 +2,7 @@ var jsonfile = require('jsonfile');
 
 global.file = 'test_reports/Accessibility_violations.json'
 global.writeOutput = writeOutput;
-global.writeOutViolations = writeOutViolations;
-global.writeOutReviews = writeOutReviews;
 global.buildOutput = buildOutput;
-
-function writeOutViolations(violations) {
-  writeOutput("Accessibility Violations: " + violations.length);
-  if (violations.length > 0) {
-    for (var i = 0; i < violations.length; i++) {
-      writeOutput('Violation: ' + violations[i].id);
-      writeOutput('Description: ' + violations[i].description);
-      writeOutput('Impact: ' + violations[i].impact);
-      writeOutput('Affected nodes:');
-      for (var j = 0; j < violations[i].nodes.length; j++) {
-        writeOutput('- ' + violations[i].nodes[j].html);
-      }
-    }
-  }
-  writeOutput('Full details: ');
-  writeOutput(JSON.stringify(violations));
-}
-
-function writeOutReviews(incomplete) {
-  writeOutput("Needs review: " + incomplete.length);
-  if (incomplete.length > 0) {
-    writeOutput(JSON.stringify(incomplete));
-    writeOutput('----');
-  }
-  else{
-    writeOutput('----');
-  }
-}
 
 function writeOutput(content, spaces) {
   spaces = spaces || 0;
@@ -65,4 +35,5 @@ function buildOutput(pageHeader, results) {
   outputContent.reviewCount = results.incomplete.length;
   outputContent.reviewDetails = JSON.stringify(results.incomplete);
   writeOutput(outputContent);
+  writeOutput('\n----\n');
 }
