50f497d99a76cc9f83ace8e39b01ed9050ad724e
Alexis Agostini <alexis.agostini@methods.co.uk>
2017-06-22 17:00:44 +0100

    adds d3 package, changes investmentChart to a module required in dit functions and updates code to use d3 v4

--- src/js/dit-functions.js
+++ src/js/dit-functions.js
@@ -1,6 +1,7 @@
 var geoLocation = require('./geo-location')
 var form = require('./form')
 var equalheight = require('./equalHeight')
+var renderInvestmentChart = require('./investment-chart')
 var logger = require('./logger')('DIT Functions')
 var debug = logger.debug
 var error = logger.error
@@ -61,6 +62,7 @@ function onLoaded() {
     playVid()
     jsEnhanceExternalLinks()
     addAltTrackingPixel()
+    renderInvestmentChart()
   } catch (e) {
     error('On loaded failed!', e)
   }
