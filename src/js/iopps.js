98c27035a519adfe74d5ad4c7987a084c6ace1b3
Alexis Agostini <alexis.agostini@methods.co.uk>
2017-07-05 16:33:30 +0100

    adds where-in-the-uk pages

--- src/js/iopps.js
+++ src/js/iopps.js
@@ -5,13 +5,13 @@ function ioppsToggles() {
     $('#sector-list').show();
   });
 
-  $('.sector').on('click', function (event) {
-    var sectorNames = $('.sector-name');
-    for (var i = 0;i < sectorNames.length; i++) {
-      sectorNames[i].innerText = event.target.dataset.sector;
-    }
-    $('#sector-list').hide();
-  });
+  // $('.sector').on('click', function (event) {
+  //   var sectorNames = $('.sector-name');
+  //   for (var i = 0;i < sectorNames.length; i++) {
+  //     sectorNames[i].innerText = event.target.dataset.sector;
+  //   }
+  //   $('#sector-list').hide();
+  // });
 
   $('.filter-toggle').on('click', function(event) {
     $('#filter-list').show();
